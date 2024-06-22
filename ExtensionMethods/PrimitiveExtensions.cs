dd4ff7374c70980f73c9710d0fba1308a7e9c349
SalvatoreAmaddio <141591986+SalvatoreAmaddio@users.noreply.github.com>
2024-06-22 02:31:08 +0100

    update

--- ExtensionMethods/PrimitiveExtensions.cs
+++ ExtensionMethods/PrimitiveExtensions.cs
@@ -8,5 +8,15 @@
             string restOf = str.Substring(1);
             return FirstLetter.ToString().ToUpper() + restOf;
         }
+
+        public static string ReplaceCharAt(this string input, int position, char newChar)
+        {
+            if (position < 0 || position >= input.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(position), "Position is out of range.");
+            }
+
+            return input.Substring(0, position) + newChar + input.Substring(position + 1);
+        }
     }
 }
